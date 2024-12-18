10491fb9da237811cd6ca06148667f84f60ca7c1
hsw <tls8204@gmail.com>
2024-12-18 16:46:20 +0900

    간단한 웹 소켓 통신 구현

--- 6. Socket - Web Socket Live Chat/chat-server.js
+++ 6. Socket - Web Socket Live Chat/chat-server.js
@@ -1,22 +1,29 @@
-// chat-server.js
+// node chat-server.js
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('A user connected'); 
+
   socket.on('chat message', (msg) => {
+    console.log('Message: ' + msg);
     io.emit('chat message', msg);
   });
+
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log('A user disconnected');
   });
 });
 
 server.listen(3000, () => {
-  console.log('Chat server running at <http://localhost:3000>');
+  console.log('Listening on http://localhost:3000');
 });
