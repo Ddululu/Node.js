7c3ef91346bf402984f753a4a94c1b0de82e802c
hsw <tls8204@gmail.com>
2024-12-18 11:17:53 +0900

    HTTP 모듈로 간단한 웹서버 만들기

--- 1. HTTP - Simple Web Server/Readme.md
+++ 1. HTTP - Simple Web Server/Readme.md
@@ -1,13 +1,4 @@
 # 1. Hello World 서버 구축
-### 설명
-
-Node.js로 간단한 HTTP 서버를 생성하여 "Hello World" 메시지를 반환하는 예제입니다.
-
-### 목표
-
-- Node.js 환경 설정 및 기본 서버 구축 방법 이해
-- HTTP 모듈 사용법 익히기
-
 ### 테스트
 
 - 터미널(bash, cmd, powershell) 실행
--- 1. HTTP - Simple Web Server/hello-world.js
+++ 1. HTTP - Simple Web Server/hello-world.js
@@ -1,14 +1,13 @@
-// node hello-world.js
-const http = require('http'); // Node.js 기본 내장 웹서버 모듈
+// hello-world.js
+const http = require('http');
+const server = http.createServer(requestListener);
 
-// HTTP 요청에 대한 응답을 생성하는 서버를 생성한다.
-const server = http.createServer((req, res) => {
-  res.statusCode = 200; // HTTP 상태 코드 설정 (200 = OK)
-  res.setHeader('Content-Type', 'text/plain'); // 응답 헤더 설정
-  res.end('Hello World\n'); // 응답 본문 반환
-});
-
-// 서버 실행: 포트 3000, 주소 127.0.0.1
 server.listen(3000, '127.0.0.1', () => {
   console.log('Server running at http://127.0.0.1:3000/');
 });
+
+function requestListener(req, res){
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+}
